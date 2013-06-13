9bc8ec005596f90aca6e9003c32b43193f82b50d
Doyler <dev@doyler.net>
2013-06-13 14:29:54 -0400

    Initial check-in
    Initial check-in, testing Git, just prints out a 3x3 board

---
+++ tictactoe.py
@@ -0,0 +1,17 @@
+import random
+
+def drawBoard(board):
+    print('   |   |')
+    print(' ' + board[6] + ' | ' + board[7] + ' | ' + board[8])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[3] + ' | ' + board[4] + ' | ' + board[5])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[0] + ' | ' + board[1] + ' | ' + board[2])
+    print('   |   |')
+
+theBoard = [' '] * 9
+drawBoard(theBoard)
