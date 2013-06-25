6fe2deea4cde4318e8fa98bc86c53c35a12030d5
doyler <dev@doyler.net>
2013-06-25 14:05:18 -0400

    Comment removal
    Just removing the comments from the sample setup.py file and cleaning it
    up

--- setup.py
+++ setup.py
@@ -2,13 +2,9 @@ from distutils.core import setup
 import py2exe
 
 setup(
-    # The first three parameters are not required, if at least a
-    # 'version' is given, then a versioninfo resource is built from
-    # them and added to the executables.
     version = "1.0.0",
     description = "A Tic-Tac-Toe game written in Python",
     name = "Doyler's Tic-Tac-Toe v1.0",
 
-    # targets to build
     console = ["tictactoe.py"],
-    )
+)
